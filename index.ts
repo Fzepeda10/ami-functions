3418f1da040f59d55c42de12b7e43e9ae45a604d
Fernando Enrique Zepeda Castellanos <fernando.zepeda@seguritech.com>
2020-07-28 19:03:14 -0500

    created functions

--- index.ts
+++ index.ts
@@ -1,13 +1,32 @@
+/**
+ * @author Fernando Enrique Zepeda Castellanos.
+ */
 import { promisify } from 'util';
-type AMI = {
+export type AMI = {
     port: string,
     host: string,
     user: string,
     password: string,
     event: boolean
 }
-export function originateCall(ami: AMI, AGENT: String, SECRET: String, QUEUES: String, TYPE: String, EXT: String, PAUSE: String) {
-    const manager = require('asterisk-manager')(ami.port, ami.host, ami.user, ami.password, ami.event);
+export type PBXExtension = {
+    type: string,
+    extension: string,
+    status: string
+}
+/**
+ * Originar llamada para iniciar session en la cola de llamadas.
+ * @param {AMI} CONN Objeto con parametros de conexion AMI.
+ * @param {string} AGENT Agente telefonico.
+ * @param {string} EXT Extension registrada en el PBX.
+ * @param {string} QUEUES Colas de llamada (MQ).
+ * @param {string} TYPE Tipo de tecnologia.
+ * @param {string} SECRET Contrasenia en MD5.
+ * @param {string} PAUSE Indica si inicia en pausa el agente.
+ * @returns Respuesta de la ejecucion AMI.
+ */
+export function originateCall(CONN: AMI, AGENT: string, EXT: string, QUEUES: string, TYPE: string, SECRET: string, PAUSE: string) {
+    const manager = require('asterisk-manager')(CONN.port, CONN.host, CONN.user, CONN.password, CONN.event);
     try {
         manager.action({
             'Action': 'Originate',
@@ -28,8 +47,155 @@ export function originateCall(ami: AMI, AGENT: String, SECRET: String, QUEUES: S
             }
         }, function () {
             manager.disconnect();
+            return { response: "success" };
         });
     } catch (e) {
         manager.disconnect();
+        return { response: "error", data: e };
+    }
+}
+/**
+ * Agregar un agente a la cola de llamadas.
+ * @param {AMI} CONN Objeto con parametros de conexion AMI.
+ * @param {string} AGENT Agente telefonico.
+ * @param {string} EXT Extension registrada en el PBX.
+ * @param {string} QUEUE Cola de llamada (MQ).
+ * @param {string} TYPE_SESSION Tipo de Session API || Manual.
+ * @param {string} PAUSE Indica si inicia en pausa el agente.
+ * @returns "response": "success" || "error".
+ */
+export async function queueAdd(CONN: AMI, UNIQUEID: string, AGENT: string, EXT: string, QUEUE: string, PAUSE: string, TYPE_SESSION: string) {
+    const manager = require('asterisk-manager')(CONN.port, CONN.host, CONN.user, CONN.password, CONN.event);
+    const manager_action = promisify(manager.action);
+    try {
+        await manager_action({
+            'Action': 'QueueAdd',
+            'Queue': QUEUE,
+            'Interface': EXT,
+            'MemberName': AGENT,
+            'Paused': PAUSE
+        })
+        manager.disconnect();
+        return { response: "success" };
+    } catch (e) {
+        manager.disconnect();
+        return { response: "error", data: e };
+    }
+}
+export async function queuePause(CONN: AMI, AGENT: string, EXT: string, TYPE: string, PAUSE: string, REASON: string) {
+    const manager = require('asterisk-manager')(CONN.port, CONN.host, CONN.user, CONN.password, CONN.event);
+    const manager_action = promisify(manager.action);
+    try {
+        await manager_action({
+            'action': 'QueuePause',
+            'Interface': `${TYPE}/${EXT}`,
+            'Paused': PAUSE,
+            'Reason': REASON
+        });
+        manager.disconnect();
+        return { response: "success" };
+    } catch (e) {
+        manager.disconnect();
+        return { response: "error", data: e };
+    }
+}
+export async function queueRemove(CONN: AMI, AGENT: string, EXT: string, QUEUE: string, TYPE_SESSION: string) {
+    const manager = require('asterisk-manager')(CONN.port, CONN.host, CONN.user, CONN.password, CONN.event);
+    const manager_action = promisify(manager.action);
+    try {
+        await manager_action({ 'action': 'QueueRemove', 'Interface': "SIP/" + EXT, 'Queue': QUEUE });
+        manager.disconnect();
+        return { response: "success" };
+    } catch (e) {
+        manager.disconnect();
+        return { response: "error", data: e };
+    }
+}
+export async function extensionState(CONN: AMI, AGENT: string, EXT: string) {
+    const manager = require('asterisk-manager')(CONN.port, CONN.host, CONN.user, CONN.password, CONN.event);
+    const manager_action = promisify(manager.action);
+    let pbx_extension: PBXExtension = { type: '', extension: '', status: '' };
+    try {
+        const manager_result = await manager_action({ 'Action': 'ExtensionState', 'Exten': EXT })
+        let type = manager_result.hint.split("/");
+        pbx_extension.type = type[0];
+        pbx_extension.extension = manager_result.exten;
+        pbx_extension.status = manager_result.statustext;
+        manager.disconnect();
+        return pbx_extension;
+    } catch (e) {
+        manager.disconnect();
+        return { response: "error", data: e };
+    }
+
+}
+export async function queueLog(CONN: AMI, AGENT: string, EXT: string, QUEUE: string, EVENT: string, TYPE_SESSION: string, TYPE: string) {
+    const manager = require('asterisk-manager')(CONN.port, CONN.host, CONN.user, CONN.password, CONN.event);
+    const manager_action = promisify(manager.action);
+    try {
+        await manager_action({
+            'Action': 'QueueLog',
+            'Queue': QUEUE,
+            'Event': EVENT,
+            'Uniqueid': 'SEGURITECH',
+            'Interface': `${TYPE}${EXT}`,
+            'Message': `${AGENT}|${TYPE_SESSION}`
+        });
+        manager.disconnect();
+        return { response: "success" };
+    } catch (e) {
+        manager.disconnect();
+        return { response: "error", data: e };
+    }
+}
+export async function BDPut(CONN: AMI, FAMILY: string, KEY: string, VALUE: string) {
+    const manager = require('asterisk-manager')(CONN.port, CONN.host, CONN.user, CONN.password, CONN.event);
+    const manager_action = promisify(manager.action);
+    try {
+        let manager_result = await manager_action({
+            'Action': 'DBPut',
+            'Family': FAMILY,
+            'Key': KEY,
+            'Val': VALUE
+        });
+        manager.disconnect();
+        return manager_result;
+    } catch (e) {
+        manager.disconnect();
+        return { response: "error", data: e };
+    }
+}
+export async function BDDel(CONN: AMI, FAMILY: string, KEY: string, VALUE: string) {
+    const manager = require('asterisk-manager')(CONN.port, CONN.host, CONN.user, CONN.password, CONN.event);
+    const manager_action = promisify(manager.action);
+    try {
+        let manager_result = await manager_action({
+            'Action': 'DBDel',
+            'Family': FAMILY,
+            'Key': KEY,
+            'Val': VALUE
+        });
+        manager.disconnect();
+        return manager_result;
+    } catch (e) {
+        manager.disconnect();
+        return { response: "error", data: e };
+    }
+}
+export async function BDGet(CONN: AMI, FAMILY: string, KEY: string, VALUE: string) {
+    const manager = require('asterisk-manager')(CONN.port, CONN.host, CONN.user, CONN.password, CONN.event);
+    const manager_action = promisify(manager.action);
+    try {
+        let manager_result = await manager_action({
+            'Action': 'DBGet',
+            'Family': FAMILY,
+            'Key': KEY,
+            'Val': VALUE
+        });
+        manager.disconnect();
+        return manager_result;
+    } catch (e) {
+        manager.disconnect();
+        return { response: "error", data: e };
     }
 }
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,10 +1,73 @@
-declare type AMI = {
+export declare type AMI = {
     port: string;
     host: string;
     user: string;
     password: string;
     event: boolean;
 };
-export declare function originateCall(ami: AMI, AGENT: String, SECRET: String, QUEUES: String, TYPE: String, EXT: String, PAUSE: String): void;
-export {};
+export declare type PBXExtension = {
+    type: string;
+    extension: string;
+    status: string;
+};
+/**
+ * Originar llamada para iniciar session en la cola de llamadas.
+ * @param {AMI} CONN Objeto con parametros de conexion AMI.
+ * @param {string} AGENT Agente telefonico.
+ * @param {string} EXT Extension registrada en el PBX.
+ * @param {string} QUEUES Colas de llamada (MQ).
+ * @param {string} TYPE Tipo de tecnologia.
+ * @param {string} SECRET Contrasenia en MD5.
+ * @param {string} PAUSE Indica si inicia en pausa el agente.
+ * @returns Respuesta de la ejecucion AMI.
+ */
+export declare function originateCall(CONN: AMI, AGENT: string, EXT: string, QUEUES: string, TYPE: string, SECRET: string, PAUSE: string): {
+    response: string;
+    data: any;
+} | undefined;
+/**
+ * Agregar un agente a la cola de llamadas.
+ * @param {AMI} CONN Objeto con parametros de conexion AMI.
+ * @param {string} AGENT Agente telefonico.
+ * @param {string} EXT Extension registrada en el PBX.
+ * @param {string} QUEUE Cola de llamada (MQ).
+ * @param {string} TYPE_SESSION Tipo de Session API || Manual.
+ * @param {string} PAUSE Indica si inicia en pausa el agente.
+ * @returns "response": "success" || "error".
+ */
+export declare function queueAdd(CONN: AMI, UNIQUEID: string, AGENT: string, EXT: string, QUEUE: string, PAUSE: string, TYPE_SESSION: string): Promise<{
+    response: string;
+    data?: undefined;
+} | {
+    response: string;
+    data: any;
+}>;
+export declare function queuePause(CONN: AMI, AGENT: string, EXT: string, TYPE: string, PAUSE: string, REASON: string): Promise<{
+    response: string;
+    data?: undefined;
+} | {
+    response: string;
+    data: any;
+}>;
+export declare function queueRemove(CONN: AMI, AGENT: string, EXT: string, QUEUE: string, TYPE_SESSION: string): Promise<{
+    response: string;
+    data?: undefined;
+} | {
+    response: string;
+    data: any;
+}>;
+export declare function extensionState(CONN: AMI, AGENT: string, EXT: string): Promise<PBXExtension | {
+    response: string;
+    data: any;
+}>;
+export declare function queueLog(CONN: AMI, AGENT: string, EXT: string, QUEUE: string, EVENT: string, TYPE_SESSION: string, TYPE: string): Promise<{
+    response: string;
+    data?: undefined;
+} | {
+    response: string;
+    data: any;
+}>;
+export declare function BDPut(CONN: AMI, FAMILY: string, KEY: string, VALUE: string): Promise<any>;
+export declare function BDDel(CONN: AMI, FAMILY: string, KEY: string, VALUE: string): Promise<any>;
+export declare function BDGet(CONN: AMI, FAMILY: string, KEY: string, VALUE: string): Promise<any>;
 //# sourceMappingURL=index.d.ts.map
