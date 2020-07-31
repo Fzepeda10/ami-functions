ceabef51f12531d5553af42c347931f542fe575b
Fernando Enrique Zepeda Castellanos <fernando.zepeda@seguritech.com>
2020-07-31 14:22:55 -0500

    contructor and document completed

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,12 @@
+/**
+ * @exports
+ * @type {AMI}
+ * @param {string} port Puerto de conexión.
+ * @param {string} host Host de conexión.
+ * @param {string} user Usuario AMI.
+ * @param {string} password Contrasenia AMI.
+ * @param {boolean} event Eventos AMI.
+ */
 export declare type AMI = {
     port: string;
     host: string;
@@ -5,6 +14,13 @@ export declare type AMI = {
     password: string;
     event: boolean;
 };
+/**
+ * @exports
+ * @type {PBXExtension}
+ * @param {string} type Tipo de tecnologia (SIP,IAX2,Etc.).
+ * @param {string} extension Numero de extension.
+ * @param {string} status Estatus de la extension.
+ */
 export declare type PBXExtension = {
     type: string;
     extension: string;
@@ -16,7 +32,7 @@ export declare type PBXExtension = {
  * @param {string} AGENT Agente telefonico.
  * @param {string} EXT Extension registrada en el PBX.
  * @param {string} QUEUES Colas de llamada (MQ).
- * @param {string} TYPE Tipo de tecnologia.
+ * @param {string} TYPE Tipo de tecnologia (SIP,IAX2,Etc.).
  * @param {string} SECRET Contrasenia en MD5.
  * @param {string} PAUSE Indica si inicia en pausa el agente.
  * @returns Respuesta de la ejecucion AMI.
@@ -26,14 +42,15 @@ export declare function originateCall(CONN: AMI, AGENT: string, EXT: string, QUE
     data: any;
 } | undefined;
 /**
- * Agregar un agente a la cola de llamadas.
+ * Agrega una extension a la cola de llamadas.
+ * @async
+ * @exports
  * @param {AMI} CONN Objeto con parametros de conexion AMI.
  * @param {string} AGENT Agente telefonico.
  * @param {string} EXT Extension registrada en el PBX.
  * @param {string} QUEUE Cola de llamada (MQ).
  * @param {string} TYPE_SESSION Tipo de Session API || Manual.
  * @param {string} PAUSE Indica si inicia en pausa el agente.
- * @returns "response": "success" || "error".
  */
 export declare function queueAdd(CONN: AMI, UNIQUEID: string, AGENT: string, EXT: string, QUEUE: string, PAUSE: string, TYPE_SESSION: string): Promise<{
     response: string;
@@ -42,13 +59,35 @@ export declare function queueAdd(CONN: AMI, UNIQUEID: string, AGENT: string, EXT
     response: string;
     data: any;
 }>;
-export declare function queuePause(CONN: AMI, AGENT: string, EXT: string, TYPE: string, PAUSE: string, REASON: string): Promise<{
+/**
+ * Pausar una extension de la cola de llamadas.
+ * @async
+ * @exports
+ * @param {AMI} CONN Conexion AMI.
+ * @param {string} AGENT Agente telefonico.
+ * @param {string} EXT Extension registrada en el PBX.
+ * @param {string} TYPE Tipo de tecnologia (SIP,IAX2,Etc.).
+ * @param {boolean} PAUSE Selecciona el estatus de pausa.
+ * @param {string} REASON Motivo de la pausa.
+ * @returns
+ */
+export declare function queuePause(CONN: AMI, AGENT: string, EXT: string, TYPE: string, PAUSE: boolean, REASON: string): Promise<{
     response: string;
     data?: undefined;
 } | {
     response: string;
     data: any;
 }>;
+/**
+ * Remueve una extension de la cola de llamadas.
+ * @async
+ * @export
+ * @param {AMI} CONN Conexion AMI.
+ * @param {string} AGENT Agente telefonico.
+ * @param {string} EXT Extension registrada en el PBX.
+ * @param {string} QUEUE Cola de llamadas.
+ * @param {string} TYPE_SESSION Tipo de session (API/MANUAL).
+ */
 export declare function queueRemove(CONN: AMI, AGENT: string, EXT: string, QUEUE: string, TYPE_SESSION: string): Promise<{
     response: string;
     data?: undefined;
@@ -56,10 +95,31 @@ export declare function queueRemove(CONN: AMI, AGENT: string, EXT: string, QUEUE
     response: string;
     data: any;
 }>;
+/**
+ * Estatus de la extension
+ * @async
+ * @export
+ * @param {AMI} CONN Conexion AMI.
+ * @param {string} AGENT Agente telefonico.
+ * @param {string} EXT Extension registrada en el PBX.
+ * @return {PBXExtension} pbx_extension.
+ */
 export declare function extensionState(CONN: AMI, AGENT: string, EXT: string): Promise<PBXExtension | {
     response: string;
     data: any;
 }>;
+/**
+ * Generar un log en queuelog de Asterisk PBX.
+ * @async
+ * @export
+ * @param {AMI} CONN Conexion AMI.
+ * @param {string} AGENT Agente telefonico.
+ * @param {string} EXT Extension registrada en el PBX.
+ * @param {string} QUEUE Cola de llamadas.
+ * @param {string} EVENT Evento a registrar.
+ * @param {string} TYPE_SESSION Tipo de session (API/MANUAL).
+ * @param {string} TYPE Tipo de tecnologia (SIP,IAX2,Etc.).
+ */
 export declare function queueLog(CONN: AMI, AGENT: string, EXT: string, QUEUE: string, EVENT: string, TYPE_SESSION: string, TYPE: string): Promise<{
     response: string;
     data?: undefined;
@@ -67,7 +127,34 @@ export declare function queueLog(CONN: AMI, AGENT: string, EXT: string, QUEUE: s
     response: string;
     data: any;
 }>;
+/**
+ * Crear o actualizar un registro en BBDD Asterisk.
+ * @async
+ * @export
+ * @param {AMI} CONN Conexion AMI.
+ * @param {string} FAMILY Familia del objeto a modificar.
+ * @param {string} KEY Llave.
+ * @param {string} VALUE Valor a modificar.
+ */
 export declare function BDPut(CONN: AMI, FAMILY: string, KEY: string, VALUE: string): Promise<any>;
+/**
+ * Eliminar un registro en BBDD Asterisk.
+ * @async
+ * @export
+ * @param {AMI} CONN Conexion AMI.
+ * @param {string} FAMILY Familia del objeto a modificar.
+ * @param {string} KEY Llave.
+ * @param {string} VALUE Valor a modificar.
+ */
 export declare function BDDel(CONN: AMI, FAMILY: string, KEY: string, VALUE: string): Promise<any>;
+/**
+ * Obtener un registro en BBDD Asterisk.
+ * @async
+ * @export
+ * @param {AMI} CONN Conexion AMI.
+ * @param {string} FAMILY Familia del objeto a modificar.
+ * @param {string} KEY Llave.
+ * @param {string} VALUE Valor a modificar.
+ */
 export declare function BDGet(CONN: AMI, FAMILY: string, KEY: string, VALUE: string): Promise<any>;
 //# sourceMappingURL=index.d.ts.map
